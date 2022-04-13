6ed1ee244876f3b372e549b4318be3824f7030ab
SingleDog <47451036+xDDoubleTea@users.noreply.github.com>
2022-04-13 11:57:02 +0800

    Add files via upload

--- car_car_2/car_car_2.ino
+++ car_car_2/car_car_2.ino
@@ -8,6 +8,81 @@ long distance = 0;
 long echo_time = 0;
 int test_time = 10;
 
+#define MotorA_I1     8  //定義 I1 接腳
+#define MotorA_I2     9  //定義 I2 接腳
+#define MotorB_I3     10  //定義 I3 接腳
+#define MotorB_I4     11 //定義 I4 接腳
+#define MotorA_PWMA   6 //定義 ENA (PWM調速) 接腳
+#define MotorB_PWMB   5 //定義 ENB (PWM調速) 接腳
+
+///int AnalogPin_R = 3; 
+//LiquidCrystal_I2C lcd(0x27,16,2);
+
+
+int r_ana,l_ana,rmp,lmp;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  //lcd.init();
+  //lcd.backlight();
+  
+  pinMode(MotorA_I1,  OUTPUT);
+  pinMode(MotorA_I2,  OUTPUT);
+  pinMode(MotorB_I3,  OUTPUT);
+  pinMode(MotorB_I4,  OUTPUT);
+//  pinMode(AnalogPin_R, INPUT);
+  //pinMode(AnalogPin_L, INPUT);
+  pinMode(DigitalPin_R, INPUT);
+  pinMode(DigitalPin_L, INPUT);
+  
+  
+  pinMode(MotorA_PWMA, OUTPUT);
+  pinMode(MotorB_PWMB, OUTPUT);
+  
+
+}
+
+
+void forward(int a)    // 前進
+{
+    digitalWrite(MotorA_I1,HIGH);   
+    digitalWrite(MotorA_I2,LOW);
+    digitalWrite(MotorB_I3,LOW);   
+    digitalWrite(MotorB_I4,HIGH);
+    delay(100);
+}
+
+void turnL(int d)    
+{
+    digitalWrite(MotorA_I1,LOW);    
+    digitalWrite(MotorA_I2,HIGH);
+    digitalWrite(MotorB_I3,LOW);   
+    digitalWrite(MotorB_I4,HIGH);
+    delay(d * 100);
+}
+
+void turnR(int e)    
+{
+    digitalWrite(MotorA_I1,HIGH);   
+    digitalWrite(MotorA_I2,LOW);
+    digitalWrite(MotorB_I3,HIGH);    
+    digitalWrite(MotorB_I4,LOW);
+    delay(e * 100);
+}    
+
+void stopRL(int f)  //停止
+{
+    digitalWrite(MotorA_I1,HIGH);   //馬達停止轉動
+    digitalWrite(MotorA_I2,HIGH);
+    digitalWrite(MotorB_I3,HIGH);   //馬達停止轉動
+    digitalWrite(MotorB_I4,HIGH);
+    delay(f * 100);
+}
+
+
+
 void ultrasound_sensor(){
   digitalWrite(trigger, LOW);
   digitalWrite(trigger, HIGH);
