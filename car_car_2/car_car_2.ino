974b669ae28fde9887272fa307a2b661e1426e6b
SingleDog <47451036+xDDoubleTea@users.noreply.github.com>
2022-04-13 10:56:39 +0800

    Add files via upload

---
+++ car_car_2/car_car_2.ino
@@ -0,0 +1,13 @@
+const int trigger = 2;
+const int echo = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
